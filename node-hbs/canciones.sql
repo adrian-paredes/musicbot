4cca5f9f100f6f0cfb3cad4bb0c118dc2ac6aac7
adrian-paredes <51762953+adrian-paredes@users.noreply.github.com>
2022-07-09 01:25:48 -0500

    Add files via upload

--- node-hbs/canciones.sql
+++ node-hbs/canciones.sql
@@ -1,6 +1,15 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+CREATE TABLE IF NOT EXISTS aadmin (
+  AdminId int(5) NOT NULL AUTO_INCREMENT,
+  AdminUserName varchar(20) DEFAULT NULL,
+  AdminPassword varchar(20) DEFAULT NULL,
+  PRIMARY KEY (AdminId)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+
+INSERT INTO aadmin(AdminId, AdminUserName, AdminPassword) VALUES 
+(1, 'MainAdmin123', '&bebitofiufiu2022%');
 
 CREATE TABLE IF NOT EXISTS musica (
   cancion_id int(10) NOT NULL AUTO_INCREMENT,
@@ -10,12 +19,11 @@ CREATE TABLE IF NOT EXISTS musica (
   emocion varchar(20) DEFAULT NULL,
   genero varchar(20) DEFAULT NULL,
   PRIMARY KEY (cancion_id)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;  
 
-
-INSERT INTO musica(cancion_id, cancion_name, artista, link, emocion, genero) VALUES
-(1, 'Somebody That I Used To Know','Gotye','https://www.youtube.com/watch?v=8UVNT4wvIGY','tristeza','pop'),
-(2, 'A man without love','Engelbert Humperdinck','https://www.youtube.com/watch?v=HQLWSC57wsE','tristeza','clasica'),
-(3, 'Somethting In The Way','Nirvana','https://www.youtube.com/watch?v=4VxdufqB9zg','enojo','rock'),
-(4, 'And the Waltz Goes on','Andre Rieu','https://youtu.be/M57Fi19vcSI','felicidad','clasica' ),
-(5, 'Duel of the Fates','John Williams','https://www.youtube.com/watch?v=D_2bluVPsb0','sorpresa','clasica');
+LOAD DATA INFILE 'musicbot - Hoja 1.csv' 
+INTO TABLE musica 
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
